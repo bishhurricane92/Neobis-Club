cf6ffbf3d326f39e98f7b9b74ef1f7a249602f21
bishhurricane92 <bishhurricane92@gmail.com>
2020-01-14 16:31:55 +0600

    3

---
+++ lesson3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+	appBegin:
+		Console.WriteLine("==========================================================================");
+		Console.WriteLine("Lesson 3");
+		Console.WriteLine("==========================================================================");
+		Console.WriteLine();
+
+		 
+
+
+
+		Console.WriteLine();
+		Console.WriteLine("==========================================================================");
+		Console.WriteLine("Выйти из программы? [y/n]:");
+		if (Console.ReadLine().Trim().ToLower() == "n") { goto appBegin; }
+	}
+}
